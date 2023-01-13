242dc27d33b5e995706ba3bb660da7c88e1876ad
LwtCompany <hunter982019@gmail.com>
2023-01-13 20:45:23 +0500

    update: fixed error with password

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 
 const userData = [];
+const errorData = [];
 
 function Click(){
     const userDataType = {
@@ -17,43 +18,56 @@ function Click(){
             minXlength: 6
         }
     };
-    let inpName = document.getElementById("name");
-    let inpEmail = document.getElementById("email");
-    let inpPassword = document.getElementById("password");
-
-    isEmail(inpEmail.value);
-
-    if(!isAttendedDigit(inpName.value)){
-        if(minLengthCheck(inpName.value,  userDataType.name.minXlength) && minLengthCheck(inpEmail.value, userDataType.email.minXlength) &&  minLengthCheck(inpPassword.value, userDataType.password.minXlength)){
-            userDataType.name.value  = inpName.value;
-            userDataType.email.value = inpEmail.value;
-            userDataType.password.value = inpPassword.value;
-            userData.push(userDataType);
-        }
-    }
-    
+    let currentData = {};
+    currentData.name = document.getElementById("name").value;
+    currentData.email = document.getElementById("email").value;
+    currentData.password = document.getElementById("password").value;
    
-    console.log(userData)
-    inpName.value = null;
-    inpEmail.value = null;
-    inpPassword.value = null;
+    if(validationData(currentData, userDataType)){
+        userData.push(currentData);
+        // inpName.value = null;
+        // inpEmail.value = null;
+        // inpPassword.value = null;
+        console.log(userData)
+    }else{
+        console.log("Somethingwrong")
+    }
+  
 }
 
 //This function can count of length string and return true if given length equal string length othercase it returns false;
 function minLengthCheck(data, lengthx){
+    let errorDataType = {
+        data: String,
+        message: String,
+        dateTime: String
+    }
     const strLength = data.length;
     if(strLength > lengthx){
         return true;
     }else{
+        errorDataType.data = data;
+        errorDataType.message = `Berilgan uzinlikdan kichkina ${lengthx}`;
+        errorDataType.dateTime = Date();
+        errorData.push(errorDataType);
         return false;
     }
 }
 
 // returns true if digit else false. Given string.
 function isAttendedDigit(data){
-    let pattern = /[0-9]/;
-    const result =  data.match(pattern);
-    if(result){
+    let errorDataType = {
+        data: String,
+        message: String,
+        dateTime: String
+    }
+    let pattern = /[0-9]/g;
+    const result = data.match(pattern);
+    console.log({result})
+    if(result ? result.length : 0  > 0){
+        errorDataType.data = data;
+        errorDataType.message = `Raqam qatnashganlikda ${result}`;
+        errorDataType.dateTime = Date();
         return true;
     }else{
         return false;
@@ -75,21 +89,42 @@ function isEmail(data){
 
 }
 
-function checkPassword(Password){
-    if(Password.length>=8 && checkPass(Password) && Password.length<=12 ){
+function checkPass(data){
+    let pattern = /\d/g, pattern2 = /\W/g, pattern3 = /[a-z]/g, pattern4 = /[A-Z]/g,
+        test1 = data.match(pattern),
+        test2 = data.match(pattern2),
+        test3 = data.match(pattern3),
+        test4 = data.match(pattern4);
+
+    if(test1 && test2 && test3 && test4){
         return true;
-    }
-   
-    function checkPass(password){
-        let pattern = /\d/g, pattern2 = /\W/g;
-        checkNum = password.match(pattern);
-        checkChar = password.match(pattern2);
-        if(checkNum && checkChar){
-            return true;
-        }
-      
-        else{
-            return false;
-        }
+    }else{
+        return false
     }
 }
+
+function validationData(data, dataType){
+    console.log({data, dataType})
+    const allResults = [];
+    allResults.push(!isAttendedDigit(data.name));
+    allResults.push(isEmail(data.email));
+    allResults.push(checkPass(data.password));
+    const checkingLength = Object.entries(data).every((value, index, array) => {
+       return minLengthCheck(value[1], dataType[value[0]].minXlength);
+    });
+    allResults.push(checkingLength);
+
+    let finalResult = allResults.every((value, index) => {
+        return value;
+    })
+
+    console.log(errorData)
+    if(finalResult){      
+        return true;
+    }else{
+        return false;
+    }
+    
+    
+}
+
