10bcb54d809468c61cfd605b2079b7065435028a
Shohruhbek Saydaliyev <97660302+shohruhbekcorparation@users.noreply.github.com>
2023-01-19 15:38:49 +0500

    Added checkFunctions data, Fixed errors

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 function Signin(email, password) {
     let emailCurrent = email.value, passwordCurrent = password.value;
+   if(isEmail(emailCurrent) && checkPass(passwordCurrent)) {
     let result = userData.find(element => {
         if(element.email == emailCurrent && element.password == passwordCurrent)  return element;       
     });
@@ -7,6 +8,11 @@ function Signin(email, password) {
         console.log(result);
         console.log("Siz saytimizga muvoffaqiyatli kirdingiz!")
         email.value = null, password.value = null;
-     }else console.log("Bunday foydalanuvchi mavjud emas! Ro'yxatdan o'ting yoki Qaytadan tering!");
+     } 
+    } 
+    else{ console.log("Ma'lumotlar noto'g'ri kiritildi! Yoki bunday foydalanuvchi mavjud emas! Ro'yxatdan o'ting!")
+    email.value = null;
+    password.value = null;
+}
 
 }
